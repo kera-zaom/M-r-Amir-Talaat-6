564df2751a19b4aab194f6b01edb8beda53abb23
kera-zaom <moazyoure@gmail.com>
2026-08-20 23:40:38 +0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+    "https://fdqolsygigqukejlwcon.supabase.co";
+
+const SUPABASE_KEY =
+    "sb_publishable_fO1Xb-dtqq8rnGuvXcPahg_zz9WeW9x";
+
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
