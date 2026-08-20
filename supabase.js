dec2f57ecb533e7d920cde51dd9bbce4ca57c223
kera-zaom <moazyoure@gmail.com>
2026-08-21 00:32:46 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,11 +1,8 @@
-const SUPABASE_URL =
-    "https://fdqolsygigqukejlwcon.supabase.co";
+const SUPABASE_URL = "https://fdqolsygigqukejlwcon.supabase.co";
 
-const SUPABASE_KEY =
-    "sb_publishable_fO1Xb-dtqq8rnGuvXcPahg_zz9WeW9x";
+const SUPABASE_KEY = "sb_publishable_fO1Xb-dtqq8rnGuvXcPahg_zz9WeW9x";
 
-const supabaseClient =
-    window.supabase.createClient(
-        SUPABASE_URL,
-        SUPABASE_KEY
-    );
+window.supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
