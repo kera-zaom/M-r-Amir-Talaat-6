b93962927d1e471a33f9420648f5f53180984f22
kera-zaom <moazyoure@gmail.com>
2026-08-21 06:32:06 +0300

    Refactor Supabase client initialization

--- supabase.js
+++ supabase.js
@@ -1,27 +1,11 @@
-// ========================================
-// SUPABASE CONFIG
-// ========================================
-
 const SUPABASE_URL =
     "https://fdqolsygigqukejlwcon.supabase.co";
 
 const SUPABASE_KEY =
     "sb_publishable_fO1Xb-dtqq8rnGuvXcPahg_zz9WeW9x";
 
-
-// ========================================
-// CREATE CLIENT
-// ========================================
-
 const supabaseClient =
     window.supabase.createClient(
         SUPABASE_URL,
-        SUPABASE_KEY,
-        {
-            auth: {
-                autoRefreshToken: true,
-                persistSession: true,
-                detectSessionInUrl: true
-            }
-        }
+        SUPABASE_KEY
     );
